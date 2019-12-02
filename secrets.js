4b9c0401e13fc728fe7b4505811fe0ba1ba75474
Andre Birkus <birkusandre@gmail.com>
2019-12-02 11:31:57 -0600

    oath changes

--- secrets.js
+++
@@ -1,4 +0,0 @@
-process.env.GOOGLE_CLIENT_ID =
-	'615911168948-3us5oripp2g2hm3emq3lk7540dk2qb2v.apps.googleusercontent.com'
-process.env.GOOGLE_CLIENT_SECRET = 'pK7_h4iGMk9Hhc57965NLXMo'
-process.env.GOOGLE_CALLBACK = 'http://localhost:1337/auth/google/callback'
