7675210058edddc3d3d593bd66729ff0bc0da33c
Andre Birkus <birkusandre@gmail.com>
2020-01-15 16:16:35 -0600

    square changes

--- server/index.js
+++ server/index.js
@@ -63,7 +63,6 @@ const createApp = () => {
 	// auth and api routes
 	app.use('/auth', require('./auth'))
 	app.use('/api', require('./api'))
-	// app.use('/stripe', require('./stripe'))
 	app.use('/square', require('./square'))
 	app.use('/wpbookings', require('./wpbookings'))
 
