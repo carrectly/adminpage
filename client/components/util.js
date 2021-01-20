3f8d1def9441c1ec312216b322097abc24974ef5
Andre Birkus <birkusandre@gmail.com>
2021-01-19 22:00:34 -0600

    updating status array

--- client/components/util.js
+++ client/components/util.js
@@ -8,14 +8,23 @@ import {deleteContactThunk} from '../store/contacts'
 
 export const getStatusArray = () => {
 	return [
-		'booked',
-		'quote',
-		'quoted',
-		'in process',
-		'returned',
-		'invoiced',
-		'done',
+		'booked new',
+		'booked us',
+		'followed up - text',
+		'followed up - call',
+		'followed up - email',
 		'cancelled',
+		'confirmed',
+		'in process',
+		'pending work approvals',
+		'ready to be returned',
+		'returned',
+		'quote inquired by customer',
+		'quote sent to a shop',
+		'quote sent to a customer',
+		'invoiced',
+		'paid',
+		'postponed',
 	]
 }
 
