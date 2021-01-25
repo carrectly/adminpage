606e923f165f2b577e2130bceaa705705ed8fca9
Andre Birkus <birkusandre@gmail.com>
2021-01-25 12:12:30 -0600

    fixing square

--- server/square/invoices.js
+++ server/square/invoices.js
@@ -91,7 +91,7 @@ router.post('/', async (req, res, next) => {
 					},
 					payment_requests: [
 						{
-							delivery_method: 'EMAIL',
+							request_method: 'EMAIL',
 							request_type: 'BALANCE',
 							due_date: `${dueDate}`,
 							tipping_enabled: true,
