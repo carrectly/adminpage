e60fe02b43f06a8800174d667fd8671a90111e9f
Andre Birkus <birkusandre@gmail.com>
2021-02-10 22:23:35 -0600

    updating square to the new API. fixed customer search and creationg. working on order creation

--- server/square/customers.js
+++ server/square/customers.js
@@ -1,18 +1,17 @@
 const router = require('express').Router()
-var SquareConnect = require('square-connect')
-var client = SquareConnect.ApiClient.instance
-const {Order, OrderDetails, Service, Customer} = require('../db/models')
-//const config = require('../../squareconfig.json').sandbox
-// Configure OAuth2 access token for authorization: oauth2
-var oauth2 = client.authentications.oauth2
-client.basePath = process.env.squareBasePath
-oauth2.accessToken = process.env.SQUARE_TOKEN
+const {ApiError, Client, Environment} = require('square')
+const {Customer} = require('../db/models')
 
-var customers = new SquareConnect.CustomersApi()
+const client = new Client({
+	timeout: 3000,
+	environment: Environment.Production, // `Environment.Sandbox` to access sandbox resources
+	accessToken: process.env.SQUARE_TOKEN,
+})
+
+const {customersApi} = client
 
 router.post('/', async (req, res, next) => {
 	try {
-		//console.log('env square', process.env.SQUARE_TOKEN)
 		let phone = req.body.customerPhoneNumber
 
 		let cust = await Customer.findOne({
@@ -21,31 +20,42 @@ router.post('/', async (req, res, next) => {
 			},
 		})
 
-		let singlecstmr = await customers.searchCustomers({
+		let singlecstmr = await customersApi.searchCustomers({
+			limit: 2,
 			query: {
 				filter: {
-					email_address: {
+					emailAddress: {
 						exact: cust.dataValues.email,
 					},
 				},
 			},
 		})
 
-		//console.log('SINGLE CUSTOMER FOUND', singlecstmr)
-		if (!singlecstmr.customers) {
-			singlecstmr = await customers.createCustomer({
-				given_name: cust.dataValues.firstName,
-				family_name: cust.dataValues.lastName,
-				email_address: cust.dataValues.email,
-				phone_number: cust.dataValues.phoneNumber,
-			})
-			singlecstmr = singlecstmr.customer
-			singlecstmr.status = 'NEW CUSTOMER ADDED IN SQUARE'
+		if (!singlecstmr.result.customers) {
+			try {
+				let {result} = await customersApi.createCustomer({
+					givenName: cust.dataValues.firstName,
+					familyName: cust.dataValues.lastName,
+					emailAddress: cust.dataValues.email,
+					phoneNumber: cust.dataValues.phoneNumber,
+				})
+				console.log('API called successfully. Returned data: ', result)
+				singlecstmr = result.customer
+				singlecstmr.status = 'NEW CUSTOMER ADDED IN SQUARE'
+			} catch (error) {
+				if (error instanceof ApiError) {
+					console.log('Errors: ', error.errors)
+					singlecstmr.status = error.errors
+				} else {
+					console.log('Unexpected Error: ', error)
+					singlecstmr.status = error
+				}
+			}
 		} else {
-			singlecstmr = singlecstmr.customers[0]
+			singlecstmr = singlecstmr.result.customers[0]
 			singlecstmr.status = 'CUSTOMER EXISTS IN SQUARE'
 		}
-		//console.log('customer obj', singlecstmr)
+
 		res.json(singlecstmr)
 	} catch (err) {
 		next(err)
--- server/square/invoices.js
+++ server/square/invoices.js
@@ -1,36 +1,33 @@
 const router = require('express').Router()
-var SquareConnect = require('square-connect')
-var client = SquareConnect.ApiClient.instance
+const {ApiError, Client, Environment} = require('square')
 const axios = require('axios')
 const {Order} = require('../db/models')
 const moment = require('moment')
 
-//const config = require('../../squareconfig.json').sandbox
-// Configure OAuth2 access token for authorization: oauth2
-var oauth2 = client.authentications.oauth2
-client.basePath = process.env.squareBasePath
-oauth2.accessToken = process.env.SQUARE_TOKEN
+const client = new Client({
+	timeout: 3000,
+	environment: Environment.Production, // `Environment.Sandbox` to access sandbox resources
+	accessToken: process.env.SQUARE_TOKEN,
+})
 
-var orders = new SquareConnect.OrdersApi()
+const {ordersApi} = client
+
 var dueDate = moment().format('YYYY-MM-DD')
 
 router.post('/', async (req, res, next) => {
 	try {
 		let order = req.body.obj
 		let customerid = req.body.id
-		//console.log('square customer id', customerid)
 		let services = req.body.obj.services
 		let idempKey = `${order.hash}${Math.floor(Math.random() * 1000)}`
 
-		// console.log('here is our idempotency key', idempKey)
-		// console.log('here are the services', services)
 		let lineItems = []
 		services.forEach(service => {
 			let amt = Number(service.orderdetails.customerPrice) * 100
 			lineItems.push({
 				name: service.name,
 				quantity: '1',
-				base_price_money: {
+				basePriceMoney: {
 					amount: amt,
 					currency: 'USD',
 				},
@@ -42,7 +39,7 @@ router.post('/', async (req, res, next) => {
 		let orderDiscount = discountAmt
 			? [
 					{
-						amount_money: {
+						amountMoney: {
 							amount: discountAmt,
 							currency: 'USD',
 						},
@@ -52,73 +49,72 @@ router.post('/', async (req, res, next) => {
 			: null
 
 		//console.log('order discount', orderDiscount)
-		let singleordr = await orders.createOrder(
-			process.env.SQUARE_LOCATION_ID,
-			{
-				idempotency_key: idempKey,
-				order: {
-					location_id: process.env.SQUARE_LOCATION_ID,
-					reference_id: `${order.hash}`,
-					line_items: lineItems,
-					customer_id: customerid,
-					discounts: orderDiscount,
-				},
-			}
-		)
-
-		//console.log('created new order in square', singleordr)
-
-		let orderid = singleordr.order.id
-
-		let invoice = await axios({
-			method: 'post',
-			url: `${process.env.squareBasePath}/v2/invoices`,
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${oauth2.accessToken}`,
-			},
-			data: {
-				idempotency_key: idempKey,
-				invoice: {
-					order_id: orderid,
-					location_id: `${process.env.SQUARE_LOCATION_ID}`,
-					invoice_number: `${idempKey}`,
-					title: `Thank you for servicing you car with us - Carrectly - ${orderid}`,
-					primary_recipient: {
-						customer_id: customerid,
-					},
-					payment_requests: [
-						{
-							request_method: 'EMAIL',
-							request_type: 'BALANCE',
-							due_date: `${dueDate}`,
-							tipping_enabled: true,
-						},
-					],
-					description: `Hi there! 
-
-We appreciate your business and glad we were able to help. 
-Hope you liked the quality, simplicity, and convenience of our service. 
-The concierge should have walked you through everything when the car was delivered, but please call or text us if you have any questions. 
-__________
-
-Feedback:
-We are working on building a service that is easy to use and helpful to more Chicagoans, so if you've experienced any glitches, confusion, or have any suggestions for us  - please let us know so we can improve.
-If you are happy with the service, take a moment to write a review. As a small start-up, we are trying to gain visibility these are tremendously helpful to small businesses. 
-Also, if you refer a friend, we will provide both of you with some discount. All they have to do is to mention you during the booking request. 
-
-YELP: https://www.yelp.com/biz/carrectly-auto-care-chicago
-GOOGLE: http://bit.ly/2vloaPl
-
-Have a fantastic rest of the week! Thank you for servicing your car - Carrectly!`,
-				},
+		let singleordr = await ordersApi.createOrder({
+			idempotencyKey: idempKey,
+			locationId: process.env.SQUARE_LOCATION_ID,
+			order: {
+				locationId: process.env.SQUARE_LOCATION_ID,
+				referenceId: `${order.hash}`,
+				lineItems: lineItems,
+				customerId: customerid,
+				discounts: orderDiscount,
 			},
 		})
 
-		// console.log('invoice .........', invoice.data.invoice)
-		res.json(invoice)
+		console.log('created new order in square', singleordr)
+
+		// let orderid = singleordr.order.id
+
+		// 		let invoice = await axios({
+		// 			method: 'post',
+		// 			url: `${process.env.squareBasePath}/v2/invoices`,
+		// 			headers: {
+		// 				'Content-Type': 'application/json',
+		// 				Authorization: `Bearer ${oauth2.accessToken}`,
+		// 			},
+		// 			data: {
+		// 				idempotency_key: idempKey,
+		// 				invoice: {
+		// 					order_id: orderid,
+		// 					delivery_method: 'EMAIL',
+		// 					location_id: `${process.env.SQUARE_LOCATION_ID}`,
+		// 					invoice_number: `${idempKey}`,
+		// 					title: `Thank you for servicing you car with us - Carrectly - ${orderid}`,
+		// 					primary_recipient: {
+		// 						customer_id: customerid,
+		// 					},
+		// 					payment_requests: [
+		// 						{
+		// 							request_type: 'BALANCE',
+		// 							due_date: `${dueDate}`,
+		// 							tipping_enabled: true,
+		// 						},
+		// 					],
+		// 					description: `Hi there!
+
+		// We appreciate your business and glad we were able to help.
+		// Hope you liked the quality, simplicity, and convenience of our service.
+		// The concierge should have walked you through everything when the car was delivered, but please call or text us if you have any questions.
+		// __________
+
+		// Feedback:
+		// We are working on building a service that is easy to use and helpful to more Chicagoans, so if you've experienced any glitches, confusion, or have any suggestions for us  - please let us know so we can improve.
+		// If you are happy with the service, take a moment to write a review. As a small start-up, we are trying to gain visibility these are tremendously helpful to small businesses.
+		// Also, if you refer a friend, we will provide both of you with some discount. All they have to do is to mention you during the booking request.
+
+		// YELP: https://www.yelp.com/biz/carrectly-auto-care-chicago
+		// GOOGLE: http://bit.ly/2vloaPl
+
+		// Have a fantastic rest of the week! Thank you for servicing your car - Carrectly!`,
+		// 				},
+		// 			},
+		// 		})
+
+		// 		// console.log('invoice .........', invoice.data.invoice)
+		// 		console.log('success------', invoice)
+		// 		res.json(invoice.data)
 	} catch (err) {
-		res.json(err)
+		console.log('error', err)
 		next(err)
 	}
 })
