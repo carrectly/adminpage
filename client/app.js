8fcd1f511c48dc7ee6828b3493204a2298d0e319
Andre Birkus <birkusandre@gmail.com>
2021-03-13 12:51:07 -0600

    adding menu layout to home page

--- client/app.js
+++ client/app.js
@@ -2,15 +2,31 @@ import React from 'react'
 import {Navbar, MainFooter} from './components'
 import Routes from './routes'
 import history from './history'
+import {Layout, Breadcrumb} from 'antd'
+
+const {Header, Footer, Sider, Content} = Layout
 
 const App = () => {
 	return (
 		<div id='appInnerDiv'>
-			<Navbar />
-			<div className='allContent'>
-				<Routes />
-			</div>
-			<MainFooter />
+			<Layout>
+				<Header className='header'>
+					<Navbar />
+				</Header>
+				<Content>
+					<Breadcrumb style={{margin: '16px 0'}}>
+						<Breadcrumb.Item>Home</Breadcrumb.Item>
+						<Breadcrumb.Item>List</Breadcrumb.Item>
+						<Breadcrumb.Item>App</Breadcrumb.Item>
+					</Breadcrumb>
+					<div className='allContent'>
+						<Routes />
+					</div>
+				</Content>
+				<Footer>
+					<MainFooter />
+				</Footer>
+			</Layout>
 		</div>
 	)
 }
--- client/components/HomePageView/BookingsByStatus.js
+++ client/components/HomePageView/BookingsByStatus.js
@@ -1,6 +1,5 @@
-import React, {Component} from 'react'
-import {withRouter, Link} from 'react-router-dom'
-import {connect} from 'react-redux'
+import React, {useState, useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import {getActiveOrdersThunk} from '../../store/activeOrders'
 import TableOrdersByStatus from './TableOrdersByStatus'
 import {
@@ -11,80 +10,93 @@ import {
 	getPotentialLeadsStatusArray,
 } from '../util'
 
-import {Collapse} from 'antd'
-const {Panel} = Collapse
+import {Layout, Menu} from 'antd'
+import {
+	NotificationOutlined,
+	ToolOutlined,
+	DollarOutlined,
+	PhoneOutlined,
+	HourglassOutlined,
+} from '@ant-design/icons'
 
-class BookingsByStatus extends Component {
-	async componentDidMount() {
-		try {
-			await this.props.getActiveOrders()
-		} catch (err) {
-			console.log(err)
-		}
+const {Content, Sider} = Layout
+
+const BookingsByStatus = () => {
+	const dispatch = useDispatch()
+	const [render, updateRender] = useState(1)
+
+	useEffect(() => {
+		dispatch(getActiveOrdersThunk())
+	}, [])
+
+	const handleMenuClick = menu => {
+		updateRender(menu.key)
+	}
+	const orders = useSelector(state => state.activeOrders)
+
+	const actionStatusArr = getTakeActionStatusArray()
+	const workZoneStatusArr = getWorkZoneStatusArray()
+	const invoiceStatusArr = getInvoicesStatusArray()
+	const quoteStatusArr = getQuotesStatusArray()
+	const leadsStatusArr = getPotentialLeadsStatusArray()
+
+	const actionArr = orders.filter(el => actionStatusArr.includes(el.status))
+
+	const workZoneArr = orders.filter(el =>
+		workZoneStatusArr.includes(el.status)
+	)
+
+	const invoiceArr = orders.filter(el => invoiceStatusArr.includes(el.status))
+
+	const quotesArr = orders.filter(el => quoteStatusArr.includes(el.status))
+
+	const leadsArr = orders.filter(el => leadsStatusArr.includes(el.status))
+
+	const components = {
+		1: <TableOrdersByStatus ordersArray={actionArr} />,
+		2: <TableOrdersByStatus ordersArray={workZoneArr} />,
+		3: <TableOrdersByStatus ordersArray={invoiceArr} />,
+		4: <TableOrdersByStatus ordersArray={quotesArr} />,
+		5: <TableOrdersByStatus ordersArray={leadsArr} />,
 	}
 
-	render() {
-		const orders = this.props.orders || []
+	// TODO: fix padding for menu item set to 0
 
-		const actionStatusArr = getTakeActionStatusArray()
-		const workZoneStatusArr = getWorkZoneStatusArray()
-		const invoiceStatusArr = getInvoicesStatusArray()
-		const quoteStatusArr = getQuotesStatusArray()
-		const leadsStatusArr = getPotentialLeadsStatusArray()
-
-		const actionArr = orders.filter(el =>
-			actionStatusArr.includes(el.status)
-		)
-
-		const workZoneArr = orders.filter(el =>
-			workZoneStatusArr.includes(el.status)
-		)
-
-		const invoiceArr = orders.filter(el =>
-			invoiceStatusArr.includes(el.status)
-		)
-
-		const quotesArr = orders.filter(el =>
-			quoteStatusArr.includes(el.status)
-		)
-
-		const leadsArr = orders.filter(el => leadsStatusArr.includes(el.status))
-
-		return (
-			<div>
-				<Collapse defaultActiveKey={['1', '2']}>
-					<Panel header='TO TAKE ACTION' key='1'>
-						<TableOrdersByStatus ordersArray={actionArr} />
-					</Panel>
-					<Panel header='WORK ZONE' key='2'>
-						<TableOrdersByStatus ordersArray={workZoneArr} />
-					</Panel>
-					<Panel header='INVOICES' key='3'>
-						<TableOrdersByStatus ordersArray={invoiceArr} />
-					</Panel>
-					<Panel header='QUOTES' key='4'>
-						<TableOrdersByStatus ordersArray={quotesArr} />
-					</Panel>
-					<Panel header='POTENTIAL LEADS' key='5'>
-						<TableOrdersByStatus ordersArray={leadsArr} />
-					</Panel>
-				</Collapse>
-			</div>
-		)
-	}
+	return (
+		<div>
+			<Layout
+				className='site-layout-background'
+				style={{padding: '24px 0'}}>
+				<Sider className='site-layout-background' width={200}>
+					<Menu
+						mode='inline'
+						defaultSelectedKeys='1'
+						defaultOpenKeys='1'
+						onClick={handleMenuClick}
+						style={{height: '100%', padding: '0'}}>
+						<Menu.Item key='1' icon={<NotificationOutlined />}>
+							To take action
+						</Menu.Item>
+						<Menu.Item key='2' icon={<ToolOutlined />}>
+							Work Zone
+						</Menu.Item>
+						<Menu.Item key='3' icon={<DollarOutlined />}>
+							Invoices
+						</Menu.Item>
+						<Menu.Item key='4' icon={<HourglassOutlined />}>
+							Quotes
+						</Menu.Item>
+						<Menu.Item key='5' icon={<PhoneOutlined />}>
+							Potential Leads
+						</Menu.Item>
+					</Menu>
+				</Sider>
+				<Content style={{padding: '0', minHeight: 280}}>
+					{components[render]}
+				</Content>
+			</Layout>
+		</div>
+	)
 }
 
-const mapStateToProps = state => {
-	return {
-		orders: state.activeOrders,
-	}
-}
-
-const mapDispatchToProps = dispatch => {
-	return {
-		getActiveOrders: () => dispatch(getActiveOrdersThunk()),
-	}
-}
-export default withRouter(
-	connect(mapStateToProps, mapDispatchToProps)(BookingsByStatus)
-)
+export default BookingsByStatus
