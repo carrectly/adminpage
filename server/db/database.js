c1210e79a70c991bf9742ba012d23cde565d10db
Andre Birkus <birkusandre@gmail.com>
2021-03-16 16:15:12 -0500

    adding pg native

--- server/db/database.js
+++ server/db/database.js
@@ -4,7 +4,7 @@ const pkg = require('../../package.json')
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 const dbUrl =
 	process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`
-const db = new Sequelize(dbUrl, {logging: false})
+const db = new Sequelize(dbUrl, {logging: false, native: true})
 
 module.exports = db
 
