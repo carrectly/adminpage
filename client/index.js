15c3621120d512dc09ec540ad3fc5b550f459734
Andre Birkus <birkusandre@gmail.com>
2021-12-09 18:18:58 -0600

    --

--- client/index.js
+++ client/index.js
@@ -4,7 +4,6 @@ import {Provider} from 'react-redux'
 import { BrowserRouter } from "react-router-dom";
 import App from './app'
 import store from './store'
-import history from './history'
 import 'antd/dist/antd.css'
 import './components/styles/styles.scss'
 
