7271ad1c74ffc0f4b1c4a397dbf80f3e9f4ebd61
Andre Birkus <birkusandre@gmail.com>
2021-12-10 12:24:36 -0600

    webpackworking

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,44 @@
 const isDev = process.env.NODE_ENV === 'development'
-const path = require('path');
 
 module.exports = {
-	mode: isDev ? 'development' : 'production',
-	entry: './client/index.js',
-	output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'public')
-	},
-  devServer: {
-    port: 1337,
-    watchContentBase: true
+  mode: isDev ? 'development' : 'production',
+  entry: './client/index.js',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js',
   },
-	resolve: {
-		extensions: ['.js', '.jsx'],
-	},
-	devtool: 'source-map',
-	watchOptions: {
-		ignored: /node_modules/,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(jsx|js)$/,
-				exclude: /node_modules/,
-				use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', {
-                "targets": "defaults"
-              }],
-              '@babel/preset-react'
-            ]
-          }
-        }]
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					'style-loader',
-					'css-loader',
-					{
-						loader: 'sass-loader',
-						options: {
-							// Prefer `dart-sass`
-							implementation: require('sass'),
-						},
-					},
-				],
-			},
-		],
-	},
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'source-map',
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer `dart-sass`
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+    ],
+  },
 }
