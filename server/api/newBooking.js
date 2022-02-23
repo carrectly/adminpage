37f28456a882027c50d5faf707fb2934b6d3385e
Andre Birkus <andre.birkus@deep6.ai>
2022-02-23 09:23:45 -0600

    fixing api

--- server/api/newBooking.js
+++ server/api/newBooking.js
@@ -6,22 +6,24 @@ const { Customer, Order } = require('../db/models')
 router.post('/', async (req, res, next) => {
   try {
     console.log('new booking request', req.body)
-    let services = req.body.services //array
-    let newcust = req.body.customer
-    let order = req.body.order
+    let services = req.body.param.services //array
+    let newcust = req.body.param.customer
+    let order = req.body.param.order
     await Customer.findOrCreate({
       where: {
         phoneNumber: newcust.phoneNumber,
       },
-      defaults: newcust,
+      defaults: { ...newcust, location: order.zipCode },
     })
 
     let ordr = await Order.create({
       ...order,
+      pickupLocation: `${order.address} ${order.city} ${order.zipCode}`,
+      stickShift: order.transmission === 'automatic' ? false : true,
       customerPhoneNumber: newcust.phoneNumber,
     })
 
-    const addServices = async (_) => {
+    const addServices = async () => {
       for (let i = 0; i < services.length; i++) {
         // addServices allows us to pass an array of ids but will not let us set the price for each unique service
         // await ordr.addServices(serviceIds, {
