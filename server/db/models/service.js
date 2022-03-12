5b876fff02341fdcb603174be4cf002d03e02b0a
Andre Birkus <andre.birkus@deep6.ai>
2022-03-11 19:09:33 -0600

    reverting services model back to whats in prod

--- server/db/models/service.js
+++ server/db/models/service.js
@@ -2,11 +2,6 @@ const Sequelize = require('sequelize')
 const db = require('../database')
 
 const Service = db.define('service', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
   name: Sequelize.STRING,
   price: {
     type: Sequelize.DECIMAL(10, 2),
