7f5fe562241ead3844e06482e4cb18bcb4c303d7
gortovenko <pavlo.hortovenko.carrer@gmail.com>
2022-05-14 11:14:34 -0500

    updated ip

--- server/db/database.js
+++ server/db/database.js
@@ -3,7 +3,7 @@ const pkg = require('../../package.json');
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
 const dbUrl =
-  process.env.DATABASE_URL || `postgres://postgres:Postgres1234@10.124.6.78:5432/${databaseName}`;
+  process.env.DATABASE_URL || `postgres://postgres:Postgres1234@10.124.2.172:5432/${databaseName}`;
 const db = new Sequelize(dbUrl, { logging: false, native: true });
 
 module.exports = db;
