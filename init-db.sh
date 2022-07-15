f33529c854f7d9cbbea10556306a86bd97456157
Pavlo Hortovenko <59263138+gortovenko@users.noreply.github.com>
2022-07-15 13:40:58 -0500

    Delete init-db.sh

--- init-db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "########### Setting up Postgres DB ###########"
-#pg_restore -U postgres -d adminpage -v "/mnt/disk/dockerentrypoint.initdb.d/dump-latest.dump" 
-cat ./dump-latest.dump  | docker exec -i adminpage_postgres_1 pg_restore --verbose --clean --no-acl --no-owner -d adminpage -U postgres
-exit 
