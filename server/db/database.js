cee5e7f773cd0c2a84647ae782ed2f931d5d9d3b
gortovenko <javalangscr@gmail.com>
2022-07-15 13:33:50 -0500

    updated step by autoclearn cach by cf

--- server/db/database.js
+++ server/db/database.js
@@ -3,12 +3,8 @@ const pkg = require('../../package.json');
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
 const dbUrl =
-  process.env.DATABASE_URL ||
   `postgres://postgres:Postgres1234@${process.env.DB_URL}:5432/${databaseName}`;
 const db = new Sequelize(dbUrl, { logging: false, native: true });
-
-module.exports = db;
-
 // if (process.env.NODE_ENV === 'test') {
 // 	after('close database connection', () => db.close())
 // }
